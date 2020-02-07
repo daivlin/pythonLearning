7fef1a2b62ec2acf4e45d247e7b6a717021410a0
daivlin <daivlin@163.com>
2020-02-07 15:24:42 +0800

    Update downloadFromLuoow.com.py

--- 5 下载落网音乐/downloadFromLuoow.com.py
+++ 5 下载落网音乐/downloadFromLuoow.com.py
@@ -1,4 +1,9 @@
 #coding:utf-8
+'''
+Auth:daivlin
+Date:2020-02-07
+'''
+
 import re
 import urllib
 import threading
@@ -7,7 +12,7 @@ from pyquery import PyQuery as pq
 import requests as rq
 
 def parse_javascript(js):
-    '''解析js文本中的mp3数据'''
+    '''解析js文本中的mp3数据,以字典形式返回：{'name':mp3Name,'url':}'''
     patternName = re.compile(r'"name":"(.*?)"') #name
     patternMp3 = re.compile(r'"src":"(.*?.mp3)"') #mp3Url
     namesList = patternName.findall(js)
@@ -18,17 +23,21 @@ def parse_javascript(js):
     return musicDictList
 
 class LuooMusic(object):
+    '''下载落网'''
     def __init__(self,issueNum):
         self.url = "http://www.luoow.com/{}/".format(issueNum)
         self.html = pq(url = self.url)
         
     def get_title(self):
+        '''获取每一期的标题'''
         return self.html("title").text()
         
     def get_coverUrl(self):
+        '''获取每一期封面'''
         return self.html(".container .cover_img img").attr("src")
         
     def get_musicDictList(self):
+        '''获取每一期音乐列表'''
         return parse_javascript(self.html("script")[1].text)
 
 class DownloadThread(threading.Thread):
@@ -53,6 +62,7 @@ class DownloadThread(threading.Thread):
                 f.write(rq.get(self.url).content)
             #urllib.request.urlretrieve(self.url, MUSICPATH)    #乱码bug还未处理
             print("%s was downloaded"%self.name)
+            
 for i in range(993,1000):
     r = LuooMusic(i)
     name = r.get_title()
